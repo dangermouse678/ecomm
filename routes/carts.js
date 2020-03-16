de3fe882b0d4e333ed49afc359fee3819852e325
Shawn Taylor <dangermouse678@gmail.com>
2020-03-16 16:35:08 -0400

    Complete video 436

---
+++ routes/carts.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cartsRepo = require('../repositories/carts');
+
+const router = express.Router();
+
+// Receive a POST request to add an items to a cart
+router.post('/cart/products', async (req, res) => {
+   // Figure out the cart
+   let cart;
+   if (!req.session.cartId) {
+      // We don't have a cart -- we need to create one
+      // and store the cart ID on the req.session.cartId
+      // property
+      cart = await cartsRepo.create({ items: [] });
+      req.session.cartId = cart.id;
+   } else {
+      // We have a cart! Let's get it from the repository
+      cart = await cartsRepo.getOne(req.session.cartId);
+   }
+
+   const existingItem = cart.items.find(item => item.id === req.body.productId);
+   if (existingItem) {
+      // Increment quantity and save cart
+      existingItem.quantity++;
+   } else {
+      // Add new product ID to Items array
+      cart.items.push({ id: req.body.productId, quantity: 1});
+   }
+
+   await cartsRepo.update(cart.id, {
+      items: cart.items
+   });
+
+   res.send('Product added to cart');
+});
+
+// Receive a GET request to show all items in cart
+
+// Receive a POST request to delete an item from a cart
+
+module.exports = router;
